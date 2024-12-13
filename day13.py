575cb09f17dce9b642b344e5448737377c4c944e
Thomas Ip <thomas@ipthomas.com>
2024-12-13 10:22:19 +0000

    Day 13 linalg

--- day13.py
+++ day13.py
@@ -1,31 +1,18 @@
 import re
 
-from sympy import solve
-from sympy.abc import a, b
-from tqdm import tqdm
-
 inputs = open("inputs/day13.txt").read().split("\n\n")
+pos_int = lambda x: x >= 0 and abs(x - int(x)) < 1e-9
 
 
 def tokens(ax, ay, bx, by, px, py, trans=0):
-    sols = solve(
-        [a * ax + b * bx - trans - px, a * ay + b * by - trans - py], [a, b], dict=True
-    )
-
-    for sol in sols:
-        if (
-            sol[a] >= 0
-            and sol[a] == int(sol[a])
-            and sol[b] >= 0
-            and sol[b] == int(sol[b])
-        ):
-            return sol[a] * 3 + sol[b]
-    return 0
+    b = ((trans + py) * ax - (trans + px) * ay) / (by * ax - bx * ay)
+    a = ((trans + px) - b * bx) / ax
+    return int(a) * 3 + int(b) if pos_int(a) and pos_int(b) else 0
 
 
 part1 = 0
 part2 = 0
-for task in tqdm(inputs):
+for task in inputs:
     args = list(map(int, re.findall(r"\d+", task)))
     part1 += tokens(*args)
     part2 += tokens(*args, trans=10000000000000)
