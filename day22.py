b4cbd6e2833300a86bf54c4e5bf6a24b7f6384ac
Thomas Ip <thomas@ipthomas.com>
2024-12-22 16:52:58 +0000

    Day 22 optimize further

--- day22.py
+++ day22.py
@@ -1,33 +1,29 @@
-from collections import Counter
+from collections import defaultdict
 from itertools import pairwise
-
-from aoc_utils import window
 
 secrets = map(int, open("inputs/day22.txt").read().splitlines())
 
 
 def evolve(x):
-    seq = [x]
-    for _ in range(2000):
-        x = ((x * 64) ^ x) % 16777216
-        x = ((x // 32) ^ x) % 16777216
-        x = ((x * 2048) ^ x) % 16777216
-        seq.append(x)
-    return seq
+    x = (x << 6 ^ x) % 16777216
+    x = (x >> 5 ^ x) % 16777216
+    return (x << 11 ^ x) % 16777216
 
 
 part1 = 0
-part2 = Counter()
+part2 = defaultdict(int)
+
 for secret in secrets:
-    seq = evolve(secret)
+    seq = [secret] + [secret := evolve(secret) for _ in range(2000)]
     part1 += seq[-1]
     digits = [x % 10 for x in seq]
-    diffs = (b - a for a, b in pairwise(digits))
+    diffs = [b - a + 9 for a, b in pairwise(digits)]
     seen = set()
-    for changes, price in zip(window(diffs, 4), digits[4:]):
-        if changes not in seen:
-            seen.add(changes)
-            part2[changes] += price
+    for i in range(len(digits) - 4):
+        key = diffs[i] * 18**3 + diffs[i + 1] * 18**2 + diffs[i + 2] * 18 + diffs[i + 3]
+        if key not in seen:
+            seen.add(key)
+            part2[key] += digits[4 + i]
 
 print(part1)
-print(part2.most_common(1)[0][1])
+print(max(part2.values()))
