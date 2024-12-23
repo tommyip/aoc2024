3657fc15d3788e658b3b9af1e7193700dee2e37c
Thomas Ip <thomas@ipthomas.com>
2024-12-23 10:57:31 +0000

    Day 23 golf

--- day23.py
+++ day23.py
@@ -2,35 +2,20 @@ from collections import defaultdict
 from itertools import combinations
 
 inputs = open("inputs/day23.txt").read().splitlines()
-
 network = defaultdict(set)
 for line in inputs:
     a, b = line.split("-")
     network[a].add(b)
     network[b].add(a)
 
-part1 = set()
-for a, neis in network.items():
-    for b, c in combinations(neis, r=2):
-        triple = tuple(sorted((a, b, c)))
-        if (
-            any(x[0] == "t" for x in triple)
-            and {b, c} <= network[a]
-            and {a, c} <= network[b]
-            and {a, b} <= network[c]
-        ):
-            part1.add(triple)
-print(len(part1))
+
+def parties(size):
+    for node in network:
+        for friends in combinations(network[node], r=size - 1):
+            party = set(friends + (node,))
+            if all(party - {friend} <= network[friend] for friend in party):
+                yield tuple(sorted(party))
 
 
-part2 = None
-party_size = max(map(len, network.values()))
-for node in network:
-    for friends in combinations(network[node], r=party_size - 1):
-        party = set(friends + (node,))
-        if all(party - {friend} <= network[friend] for friend in party):
-            part2 = ",".join(sorted(party))
-            break
-    if part2:
-        break
-print(part2)
+print(sum(any(x[0] == "t" for x in p) for p in set(parties(3))))
+print(",".join(next(parties(len(network[a])))))
